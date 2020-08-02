df84403c27f44c27124406c35106796ea6d88f18
Dan <66588578+Artis7eeR@users.noreply.github.com>
2020-08-02 15:12:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,18 +4,18 @@ import os
 #Bot Token
 #Needed To Interact With Bot
 source="https://github.com/Artis7eeR/My-forward-Tag-Remover-Bot/bot.py"
-START_TEXT=os.environ.get("StartMsg","Hi I am A Forward Tag remover Bot \n Find My Source On Here {}".format(source))
-HELP_TEXT=os.environ.get("HelpMsg","Forward Me A File And I Will Remove The Files  \n Tag Find My Source On Here {}".format(source))
+START_TEXT=os.environ.get("StartMsg","Hi I am A Forward Tag remover Bot \n Find My Source On Here {}")
+HELP_TEXT=os.environ.get("HelpMsg","Forward Me A File And I Will Remove The Files  \n Tag Find My Source On Here {}")
 token=os.environ.get("BOT_TOKEN",None)
 
 
 #Start Message
 def start_text(u,c):
-	u.message.reply_text(START_TEXT)
+	u.message.reply_text(START_TEXT.format(source))
 
 #Help Message
 def help_text(u,c):
-	u.message.reply_text(HELP_TEXT)
+	u.message.reply_text(HELP_TEXT.format(source))
 
 #Send Document From User
 def frwrd_file(u,c):

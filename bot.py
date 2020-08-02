2171015398d71094013a924c4828f94c8a4fa997
Dan <66588578+Artis7eeR@users.noreply.github.com>
2020-08-02 15:26:50 +0530

    Added Source

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 #Bot Token
 #Needed To Interact With Bot
 source="https://github.com/Artis7eeR/My-forward-Tag-Remover-Bot/bot.py"
-START_TEXT=os.environ.get("StartMsg",f"Hi I am A Forward Tag remover Bot\nSource Code{source}")
+START_TEXT=os.environ.get("StartMsg",f"Hi I am A Forward Tag remover Bot\nSource Code: {source}")
 HELP_TEXT=os.environ.get("HelpMsg",f"Forward Me A File Video Anything and I will Remove The Forward Tag\nSource Code:{source}")
 token=os.environ.get("BOT_TOKEN",None)
 

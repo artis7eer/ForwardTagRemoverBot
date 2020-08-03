5b50b77e13bc5b23fa334f0c6387a604f52f347b
Dan <66588578+Artis7eeR@users.noreply.github.com>
2020-08-03 21:38:23 +0530

    Start and Help Text Variables Changed

--- bot.py
+++ bot.py
@@ -1,3 +1,7 @@
+#
+#(c)Dan(Artis7eer)
+#Copy With Credits
+
 from telegram.ext import Filters,Updater,MessageHandler,CommandHandler
 import os
 
@@ -5,42 +9,42 @@ import os
 #Needed To Interact With Bot
 source="https://github.com/Artis7eeR/forward-Tag-Remover-Bot"
 
-START_TEXT=os.environ.get("StartMsg",f"Hi I am A Forward Tag remover Bot\nSource Code: {source}")
-HELP_TEXT=os.environ.get("HelpMsg",f"Forward Me A File Video Anything and I will Remove The Forward Tag\nSource Code: {source}")
+START_TEXT=os.environ.get("START_TEXT",f"Hi I am A Forward Tag remover Bot\nSource Code: {source}")
+HELP_TEXT=os.environ.get("HELP_TEXT",f"Forward Me A File Video Anything and I will Remove The Forward Tag\nSource Code: {source}")
 token=os.environ.get("BOT_TOKEN",None)
 
 
 #Start Message
 def start_text(u,c):
-	u.message.reply_text(START_TEXT)
+  u.message.reply_text(START_TEXT)
 
 #Help Message
 def help_text(u,c):
-	u.message.reply_text(HELP_TEXT)
+  u.message.reply_text(HELP_TEXT)
 
 #Send Document From User
 def frwrd_file(u,c):
-	u.message.reply_document(u.message.document.file_id)
+  u.message.reply_document(u.message.document.file_id)
 
 #Send Video From User
 def frwrd_media(u,c):
-	u.message.reply_video(u.message.video.file_id)
+  u.message.reply_video(u.message.video.file_id)
 
 #Send photo From User
 def frwrd_photo(u,c):
-	u.message.reply_photo(u.message.photo[-1].file_id)
+  u.message.reply_photo(u.message.photo[-1].file_id)
 
 #Send text From User
 def frwrd_text(u,c):
-	u.message.reply_text(u.message.text)
+  u.message.reply_text(u.message.text)
 
 #Send Sticker From User
 def frwrd_sticker(u,c):
-	u.message.reply_sticker(u.message.sticker.file_id)
+  u.message.reply_sticker(u.message.sticker.file_id)
 
 #Send Sticker From User
 def frwrd_voice(u,c):
-	u.message.reply_voice(u.message.voice.file_id)
+  u.message.reply_voice(u.message.voice.file_id)
 
 updater=Updater(token,use_context=True)
 
@@ -48,12 +52,11 @@ dp=updater.dispatcher
 
 #Filtering Commands
 
+#Start Command
 dp.add_handler(CommandHandler('start',start_text))
 
-
+#Help Command
 dp.add_handler(CommandHandler('help',help_text))
-
-
 
 #Filtering Files
 dp.add_handler(MessageHandler(Filters.document,frwrd_file))

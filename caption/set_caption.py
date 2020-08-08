f07d83c9ab3cad46fff7b025f7508d3db6e55b57
Dan <66588578+Artis7eeR@users.noreply.github.com>
2020-08-08 12:39:34 +0530

    Update set_caption.py

--- caption/set_caption.py
+++ caption/set_caption.py
@@ -2,9 +2,9 @@
 def set_caption(u,c):
  if u.message.reply_to_message is not None:
    file_caption= u.message.text
-  file_type=f"{u.message.reply_to_message}"
-  if "document" in file_type:
-   u.message.reply_document(u.message.reply_to_message.document.file_id,caption=file_caption)
+   file_type=f"{u.message.reply_to_message}"
+   if "document" in file_type:
+    u.message.reply_document(u.message.reply_to_message.document.file_id,caption=file_caption)
    elif "video" in file_type:
     u.message.reply_video(u.message.reply_to_message.video.file_id,caption=file_caption)
    elif "photo" in file_type:

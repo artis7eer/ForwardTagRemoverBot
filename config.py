97adf3d8139a878d9073540162d51e5f56428d4b
Disney team <83995690+disneyteam75@users.noreply.github.com>
2021-05-18 11:36:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,6 +20,6 @@ class Config:
 	
  TOKEN=os.environ.get("BOT_TOKEN",None)
  SOURCE="https://github.com/Artis7eeR/ForwardTagRemoverBot"
- START_TEXT="Hi [{}](tg://user?id={})\nI am A Forward Tag remover Bot.Send /help To Know What I Can Do \n ©Artis7eeR"
- HELP_TEXT="Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
+ START_TEXT="Hi [{}](tg://user?id={})\nI am A  Disney Team Forward Tag remover Bot.Send /help To Know What I Can Do \n ©Disneyteam"
+ HELP_TEXT=" Bro Forward Me A File,Video,Audio,Photo or Anything And \nI will Send You the File Back\n\n`How to Set Caption?`\nReply Caption to a File,Photo,Audio,Media"
 	
